0d6e51a6f8ce2b6032c6a73579fd6ac179c0ad84
jmglueck <caravaggio4lyfe@gmail.com>
2021-01-27 12:52:10 -0800

    Edited Comments and Added ScreenManager
    I edited the comments to clarify what I meant by the use of database and also revamped the app to use screenmanager so we can use multiple pages

--- mainpage.py
+++ mainpage.py
@@ -1,14 +1,27 @@
 from kivy.app import App 
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.button import Button 
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+Builder.load_string("""
+<MainScreen>:
+    AnchorLayout:
+        Button:
+            text: 'Login'
+        Button:
+            text: 'Quit'
+            on_press: app.stop()
+""")
+
+class MainScreen(Screen):
+    pass
+
 
 class RexableApp(App):
     def build(self):
-        layout = AnchorLayout(
-            anchor_x='left', anchor_y='top')
-        butn = Button(text='Test')
-        layout.add_widget(butn)
-        return layout
+        sm = ScreenManager()
+        sm.add_widget(AnchorLayout(anchor_x='left', anchor_y='top'))
+        return sm
 
 if __name__ == '__main__':
 
--- user.py
+++ user.py
@@ -2,5 +2,5 @@
 
 class User:
     logged_in = False
-    #will include user's current weather and user's current time as variables and the user id to access the user's table in the database
+    #will include user's current weather and user's current time as variables and the user id to access the user's row in the user table in the database
     #user's table in the database will contain more permanent information
