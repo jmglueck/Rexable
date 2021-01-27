ae993b668e313828bbf6198d7485351877a7314d
jmglueck <caravaggio4lyfe@gmail.com>
2021-01-27 13:03:22 -0800

    Fixed ScreenManager and Buttons
    I fixed the buttons, added a quit button and a login button, etc. Will add new "screen" for actual login page soon.

--- mainpage.py
+++ mainpage.py
@@ -1,15 +1,23 @@
 from kivy.app import App 
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.button import Button 
+from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 
 Builder.load_string("""
 <MainScreen>:
     AnchorLayout:
+        anchor_x: 'right'
+        anchor_y: 'top'
         Button:
             text: 'Login'
+            size_hint: (.5, .5)
+    AnchorLayout:
+        anchor_x: 'left'
+        anchor_y: 'top'
         Button:
             text: 'Quit'
+            size_hint: (.5, .5)
             on_press: app.stop()
 """)
 
@@ -20,7 +28,7 @@ class MainScreen(Screen):
 class RexableApp(App):
     def build(self):
         sm = ScreenManager()
-        sm.add_widget(AnchorLayout(anchor_x='left', anchor_y='top'))
+        sm.add_widget(MainScreen(name='main_screen'))
         return sm
 
 if __name__ == '__main__':
