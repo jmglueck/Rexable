f9663627b49dd74d038b8dc501af3ced727c8106
jmglueck <caravaggio4lyfe@gmail.com>
2021-01-27 12:29:47 -0800

    Created class for user
    We need a user class to store some information like user id that we will use to access the user's table in the database

---
+++ user.py
@@ -0,0 +1,6 @@
+#User class
+
+class User:
+    logged_in = False
+    #will include user's current weather and user's current time as variables and the user id to access the user's table in the database
+    #user's table in the database will contain more permanent information
