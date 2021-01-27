29fc5e7f2fcecb1f1a10a70f489947f876a19dec
jmglueck <caravaggio4lyfe@gmail.com>
2021-01-27 01:44:25 -0800

    Create mainpage.py

---
+++ mainpage.py
@@ -0,0 +1,11 @@
+from kivy.app import App 
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.button import Button 
+
+class RexableApp(App):
+    def build(self):
+        layout = AnchorLayout(
+            anchor_x='left', anchor_y='top')
+        butn = Button(text='Test')
+        layout.add_widget(butn)
+        return layout
