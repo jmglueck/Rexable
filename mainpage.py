8eb9577d3400c595685b96a9e448da3a9edaeea5
jmglueck <caravaggio4lyfe@gmail.com>
2021-01-29 13:06:00 -0800

    Added Login Page

--- mainpage.py
+++ mainpage.py
@@ -1,17 +1,64 @@
 from kivy.app import App 
 from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button 
 from kivy.lang import Builder
+from kivy.uix.label import Label
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.textinput import TextInput
 
 Builder.load_string("""
+<LoginScreen>:
+    name: 'login_screen'
+    AnchorLayout:
+        anchor_x: 'center'
+        anchor_y: 'top'
+        BoxLayout:
+            orientation: 'vertical'
+            size_hint: (0.9, 0.7)
+            Label:
+                text: 'Username'
+                halign: 'center'
+                text_size: (75, 50)
+                size_hint: (.2, .2)
+            TextInput:
+                multiline: False
+                halign: 'center'
+                font_size: '12sp'
+                height: 30
+                size_hint: (.2, None)
+            Label:
+                text: 'Password'
+                halign: 'center'
+                text_size: (75, 50)
+                size_hint: (.2, .2)
+            TextInput:
+                multiline: False
+                halign: 'center'
+                font_size: '12sp'
+                height: 30
+                size_hint: (.2, None)
+                password_mask: 'True'
+    AnchorLayout:
+        anchor_x: 'center'
+        anchor_y: 'center'
+        size_hint: (0.7, 0.3)
+        BoxLayout:
+            padding: [15]
+            orientation: 'horizontal'
+            Button:
+                text: 'Login'
+            Button:
+                text: 'Sign Up'
 <MainScreen>:
+    name: 'main_screen'
     AnchorLayout:
         anchor_x: 'right'
         anchor_y: 'top'
         Button:
             text: 'Login'
             size_hint: (.5, .5)
+            on_release: root.manager.current = 'login_screen'
     AnchorLayout:
         anchor_x: 'left'
         anchor_y: 'top'
@@ -19,16 +66,21 @@ Builder.load_string("""
             text: 'Quit'
             size_hint: (.5, .5)
             on_press: app.stop()
+        
+
 """)
+
+class LoginScreen(Screen):
+    pass
 
 class MainScreen(Screen):
     pass
-
 
 class RexableApp(App):
     def build(self):
         sm = ScreenManager()
         sm.add_widget(MainScreen(name='main_screen'))
+        sm.add_widget(LoginScreen(name='login_screen'))
         return sm
 
 if __name__ == '__main__':
