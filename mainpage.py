672600c8c4b25ae1674912c8a8a1c04feac253b2
jmglueck <caravaggio4lyfe@gmail.com>
2021-03-01 19:22:44 -0800

    Hopefully Fixed setting_screen error
    I'm not sure, but hopefully this will fix the error we're having with "No Screen with name "settings_screen"

--- mainpage.py
+++ mainpage.py
@@ -506,7 +506,7 @@ class RexableApp(App):
         self.sm.add_widget(MainScreen(name='main_screen'))
         self.sm.add_widget(LoginScreen(name='login_screen'))
         self.sm.add_widget(SignUpScreen())
-        self.sm.add_widget(SettingsScreen(name='setting_screen'))
+        self.sm.add_widget(SettingsScreen(name='settings_screen'))
         self.sm.add_widget(ProfileScreen(name='profile_screen'))
         self.sm.add_widget(RecommendationPreferenceScreen(name='recommendation_preference_screen'))
         self.sm.add_widget(AboutScreen(name='about_screen'))
