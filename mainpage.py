f035ba6a6b93f2791f7372c3b4b2869e36605e79
jmglueck <caravaggio4lyfe@gmail.com>
2021-03-04 14:06:14 -0800

    Revert "Added RecipeScreen class"
    This reverts commit a80a5e33336bb1ae0f8cf660a5b170583bab5719.

--- mainpage.py
+++ mainpage.py
@@ -2052,16 +2052,13 @@ Builder.load_string("""
                 on_press: root.logout()
 
 
+
+
+
 <RecommendationLayout>:
     size_hint: (0.5, 0.5)
     height: 100
     width: 100
-
-<RecipeScreen>:
-    BoxLayout:
-        orientation: 'vertical'
-        Label:
-            text: 'Test'
 
 """)
 
@@ -2438,9 +2435,6 @@ class SignUpScreen(Screen):
         self.parent.current = "main_screen"
 
 
-class RecipeScreen(Screen):
-    #add RecycleView later
-    pass
 
 class SettingsScreen(Screen):
     pass
